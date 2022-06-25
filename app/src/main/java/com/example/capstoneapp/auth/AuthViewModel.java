2a9aeb06c66819fc3f62f6272646d629708cce0e
trinlrich <106702885+trinlrich@users.noreply.github.com>
2022-06-25 00:32:39 -0700

    Reset master to original before Firebase.

--- app/src/main/java/com/example/capstoneapp/auth/AuthViewModel.java
+++ app/src/main/java/com/example/capstoneapp/auth/AuthViewModel.java
@@ -1,20 +1,7 @@
 package com.example.capstoneapp.auth;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
 public class AuthViewModel extends ViewModel {
 
-    public enum AuthenticationState {
-        AUTHENTICATED, UNAUTHENTICATED;
-    }
-
-    FirebaseUserLiveData firebaseUserLiveData = new FirebaseUserLiveData();
-    LiveData<AuthenticationState> authenticationState = Transformations.map(firebaseUserLiveData, authenticatedUser -> {
-        if (authenticatedUser != null) {
-            return AuthenticationState.AUTHENTICATED;
-        }
-        return AuthenticationState.UNAUTHENTICATED;
-    });
 }
